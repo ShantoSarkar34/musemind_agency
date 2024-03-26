5b22cb6e9d6db57ce022e9110287276abcddec79
ShantoSarkar34 <shanto9070sarkar@gmail.com>
2024-03-26 14:38:40 +0600

    fix some problem

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return (
-        <section className='bg-bg w-full py-20'>
-            <div className='container mx-auto px-4'>
-                <h1>This is about section</h1>
-
-            </div>
-
-        </section>
-    );
-}
+  return (
+    <section className="bg-bg w-full py-20">
+      <div className="container mx-auto px-4">
+        <h1 className="text-center text-[22px] text-primary font-medium  leading-[150%]">
+          About us
+        </h1>
+      </div>
+    </section>
+  );
+};
 
 export default About;
