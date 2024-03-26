a3a9360dcef7ed79810e55837c5fe4827bb33677
ShantoSarkar34 <shanto9070sarkar@gmail.com>
2024-03-26 14:20:35 +0600

    scrollbar color change

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Cloned by Shanto",
+  title: "Cloned by | Shanto Sarkar",
   description: "This site is cloned by- Md Shanto Sarkar",
 };
 
